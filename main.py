e87870d33b2e8cd16163ddf7c8764f00446cb7aa
ryanrugh <ryanrugh@gmail.com>
2025-04-30 15:39:19 -0400

    initial black screen is rendering and responding to quit button

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill((0,0,0))
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
