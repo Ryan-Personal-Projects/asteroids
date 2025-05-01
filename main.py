b52b93715ae91ce65369df3654063367f0b9482b
ryanrugh <ryanrugh@gmail.com>
2025-05-01 12:16:03 -0400

    Added CircleShape and Player class, and integrated that into main game play. Player's ship works and can move appropriately

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill((0,0,0))
+        
+        updatable.update(dt)
+        screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # Limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
